d054c488a854f8435c1dd34e8853086fc1ea927f
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2022-08-08 16:20:20 +0200

    tests: add helper function for VM-related Admin API calls
    For some more higher level tests, add a function that mocks a whole
    VM with all properties, features and tags.

--- qubesadmin/tests/__init__.py
+++ qubesadmin/tests/__init__.py
@@ -22,6 +22,7 @@ import traceback
 import unittest
 
 import io
+from typing import Union, Mapping, Tuple
 
 import qubesadmin
 import qubesadmin.app
@@ -138,6 +139,55 @@ class _AssertNotRaisesContext(object):
             return False
 
 
+default_vm_properties = {
+    "autostart": ("bool", True, "False"),
+    "backup_timestamp": ("int", True, ""),
+    "debug": ("bool", True, "False"),
+    "default_user": ("str", True, "user"),
+    "dns": ("str", True, "10.139.1.1 10.139.1.2"),
+    "gateway": ("str", True, ""),
+    "gateway6": ("str", True, ""),
+    "icon": ("str", True, "appvm-green"),
+    "include_in_backups": ("bool", True, "True"),
+    "installed_by_rpm": ("bool", True, "False"),
+    "ip": ("str", True, "10.137.0.2"),
+    "ip6": ("str", True, ""),
+    "kernel": ("str", True, "5.15.52-1.fc32"),
+    "keyboard_layout": ("str", True, "us++"),
+    "klass": ("str", True, "AppVM"),
+    "label": ("label", False, "green"),
+    "mac": ("str", True, "00:16:3e:5e:6c:00"),
+    "maxmem": ("int", True, "4000"),
+    "memory": ("int", True, "400"),
+    "name": ("str", False, "testvm"),
+    "provides_network": ("bool", True, "False"),
+    "qid": ("int", False, "2"),
+    "qrexec_timeout": ("int", True, "60"),
+    "shutdown_timeout": ("int", True, "60"),
+    "start_time": ("str", True, ""),
+    "stubdom_mem": ("int", True, ""),
+    "stubdom_xid": ("str", True, "-1"),
+    "template_for_dispvms": ("bool", True, "False"),
+    "updateable": ("bool", True, "False"),
+    "uuid": ("str", False, "8fd73e95-a74b-4bf0-a87d-9978dbd1d8a4"),
+    "vcpus": ("int", True, "2"),
+    "virt_mode": ("str", True, "pvh"),
+    "visible_gateway": ("str", True, "10.137.0.1"),
+    "visible_gateway6": ("str", True, ""),
+    "visible_ip": ("str", True, "10.137.0.2"),
+    "visible_ip6": ("str", True, ""),
+    "visible_netmask": ("str", True, "255.255.255.255"),
+    "xid": ("str", True, "2"),
+    "audiovm": ("vm", True, "dom0"),
+    "default_dispvm": ("vm", False, "default-dvm"),
+    "guivm": ("vm", True, "dom0"),
+    "kernelopts": ("str", True, ""),
+    "management_dispvm": ("vm", True, "default-mgmt-dvm"),
+    "netvm": ("vm", False, "sys-firewall"),
+    "template": ("vm", False, "fedora-36"),
+}
+
+
 class QubesTest(qubesadmin.app.QubesBase):
     expected_service_calls = None
     expected_calls = None
@@ -154,6 +204,69 @@ class QubesTest(qubesadmin.app.QubesBase):
         self.actual_calls = []
         #: rpc service calls
         self.service_calls = []
+
+    def add_expected_vm(self,
+                        name: str,
+                        klass: str,
+                        properties: Mapping[str,
+                                            Union[bool,
+                                                  str,
+                                                  int,
+                                                  Tuple[str, bool, str]]],
+                        features,
+                        tags):
+        """Generate expected_calls entries to get info about a VM
+
+        :param name: name of the VM
+        :param klass: class of the VM (AppVM, TemplateVM etc)
+        :param properties: dict of properties; values can be either a value
+            directly, or tuple of (type, is_default, value)
+        :param features: dict of expected features - use 'None' as value for
+            feature that will be checked but is not present
+        :param tags: list of tags
+        :return:
+        """
+        vm_list_call = ('dom0', 'admin.vm.List', None, None)
+        vm_list = b'0\x00'
+        if vm_list_call in self.expected_calls:
+            vm_list = self.expected_calls[vm_list_call]
+        vm_list += f'{name} class={klass} state=Halted\n'.encode()
+        self.expected_calls[vm_list_call] = vm_list
+        properties_getall = b"0\x00"
+        combined_properties = default_vm_properties.copy()
+        for prop, value in properties.items():
+            if isinstance(value, tuple):
+                combined_properties[prop] = value
+            elif value is None:
+                try:
+                    del combined_properties[prop]
+                except KeyError:
+                    pass
+            elif prop in combined_properties:
+                combined_properties[prop][2] = str(value)
+            else:
+                raise KeyError(f"Unknown property '{prop}'")
+
+        if klass in ("TemplateVM", "StandaloneVM"):
+            del combined_properties["template"]
+
+        for prop, value in combined_properties.items():
+            prop_line = f"{prop} default={value[1]} type={value[0]} {value[2]}"
+            properties_getall += (prop_line + "\n").encode()
+            self.expected_calls[(name, "admin.vm.property.Get", prop, None)] = \
+                b"0\x00" + prop_line.encode()
+
+        self.expected_calls[(name, "admin.vm.feature.List", None, None)] = \
+            ("0\x00" + "".join(f"{feature}\n" for feature in features)).encode()
+        for feature, value in features:
+            self.expected_calls[(name, "admin.vm.feature.Get", feature, None)] = \
+                b"0\x00" + value.encode()
+
+        self.expected_calls[(name, "admin.vm.tag.List", None, None)] = \
+            ("0\x00" + "".join(f"{tag}\n" for tag in tags)).encode()
+        for tag in tags:
+            self.expected_calls[(name, "admin.vm.tag.Get", tag, None)] = \
+                b"0\x001"
 
     def qubesd_call(self, dest, method, arg=None, payload=None,
             payload_stream=None):
